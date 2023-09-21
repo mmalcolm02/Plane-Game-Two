008c6ecb9e508cf307537ce8a6c6a8578a6ec01e
mmalcolm02 <mmalcolm02@qub.ac.uk>
2023-09-21 12:47:14 +0100

    Initial Commit
    Previous GitHub commits failed to push so this is a second attempt at setting them up.

---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    //speed changed to a reasonable level testing with public float first
+    public float speed = 0.3f;
+    //rotationSpeed changed to a reasonable level testing with public float first
+    public float rotationSpeed = 50;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed);
+
+        // tilt the plane up/down based on up/down arrow keys - vertical input used for interactivity
+        transform.Rotate(Vector3.right, verticalInput * rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Challenge 1/Scripts/PropellarSpin.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellarSpin : MonoBehaviour
+{
+    private float propellerSpeed = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, propellerSpeed);
+    }
+}
